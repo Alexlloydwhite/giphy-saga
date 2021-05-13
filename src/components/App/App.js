811042ee6b7f05691cc7188fc3d4f2a7395c56b8
Chloe Everson <chloeliz93@gmail.com>
2021-05-13 16:35:06 -0500

    added route to favorites

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,6 +11,16 @@ function App() {
       <h1>Giphy Search!</h1>
       <Router>
         <Route exact path="/" component={SearchForm} />
+        <nav>
+          <ul>
+            <li>
+              <Link to='/favorites'>Favorites</Link>
+            </li>
+          </ul>
+        </nav>
+        <Route path="/favorites" exact>
+          <FavoriteList />
+        </Route>
       </Router>
     </div>
   );
