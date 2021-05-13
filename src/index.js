3292e03e36a466df2e1aacd886af2a449962bd46
Johnny Keple <Johnny.Keple@gmail.com>
2021-05-13 10:02:52 -0500

    pushing index changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,74 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import logger from 'redux-logger';
+import {put, takeEvery} from 'redux-saga/effects';
+import createSagaMiddleware from 'redux-saga';
+import axios from 'axios';
+
+
+//-------SAGA-------//
+
+function* getGif() {
+    try {
+        const response = yield axios.get('/api/');
+        yield put({type: 'SET_GIF', payload: response.data});
+    } catch (error) {
+        alert('Unable to get gif from server');
+        console.log('ERROR in getGif', error);
+    }
+}
+
+function* addGif(action) {
+    try {
+        yield axios.post('/api/', action.payload);
+        yield put({type: 'FETCH_GIF'});
+    } catch (error) {
+        alert('Unable to add new gif');
+        console.log('ERROR in addGif', error);
+    }
+}
+
+function* deleteGif(action) {
+    try{
+        yield axios.delete(`/api//${action.payload}`);
+        yield put({type: 'FETCH_GIF'});
+    } catch (error) {
+        alert('Unable to delete Gif');
+        console.log('ERROR in deleteing gif', error);
+    }
+}
+
+const sagaMiddleware = createSagaMiddleware();
+
+function* rootSaga() {
+    yield takeEvery('FETCH_GIF', getGif);
+    yield takeEvery('ADD_GIF', addGif);
+    yield takeEvery('DELETE_GIF', deleteGif);
+}
+
+const gifList = (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_GIF':
+            return[...state, action.payload]
+        case 'SET_GIF':
+            return action.payload
+        default:
+            return state;
+    }
+};
+
+const store = createStore (
+    combineReducers({
+        gifList
+    }),
+    applyMiddleware(sagaMiddleware, logger),
+);
+
+sagaMiddleware.run(rootSaga);
+
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
