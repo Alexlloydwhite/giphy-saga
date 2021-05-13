c63f8ac1a0cb00cfef36179276908c176387b088
Alex White <alexlloydwhite@gmail.com>
2021-05-13 14:03:42 -0500

    removing old funcs

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,8 @@
 import React from 'react';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
 import { HashRouter as Router, Route } from 'react-router-dom';
 import SearchForm from '../Search/searchform';
 
-
 function App() {
-  //create variable for accessing dispatch function 
-  const dispatch = useDispatch();
-  //
-  // useEffect(() => {
-  //   dispatch({ type: 'FETCH_GIF' })
-  // })
-
-
   return (
     <div>
       <h1>Giphy Search!</h1>
